c7b1eb6c172dd6df29a2954ac313a62c876d2f28
Viktor Kukharev <lacedon1@gmail.com>
2019-11-22 11:37:54 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const util = require('util');
+const cp = require('child_process');
+const fsExtra = require('fs-extra');
+const Koa = require('koa');
+const { zip } = require('zip-a-folder');
+const ffp = require('find-free-port');
+
+(async () => {
+  const readFile = util.promisify(fs.readFile);
+  const createDir = util.promisify(fs.mkdir);
+  const execChildProcess = util.promisify(cp.exec);
+  const findFreePort = util.promisify(ffp);
+  const tempFolderPath = './temp';
+
+  const [port, front] = await Promise.all([
+    findFreePort(process.env.port || 3000),
+    readFile('./index.html'),
+    createDir(tempFolderPath),
+  ]);
+
+  return new Koa()
+    .use(async function loadFile(ctx, next) {
+      if (!ctx.query.p) {
+        next();
+        return;
+      }
+
+      const rawPackageName = ctx.query.p.replace(/&|/, '');
+      const packageName = rawPackageName.replace(/\W/, '_');
+      const folderName = `${Date.now()}-${packageName}`;
+      const folderPath = `${tempFolderPath}/${folderName}`;
+      const archiveName = 'modules.zip';
+      const archivePath = `${folderPath}/${archiveName}`;
+
+      try {
+        await createDir(folderPath);
+        await execChildProcess('npm init -y', { cwd: folderPath });
+        await execChildProcess(`npm i ${rawPackageName} --ignore-scripts`, {
+          cwd: folderPath
+        });
+        await zip(`${folderPath}/node_modules`, archivePath);
+        const readFileStream = fs.createReadStream(archivePath);
+
+        ctx.status = 200;
+        ctx.type = 'application/zip';
+        ctx.set('Content-Disposition', `attachment; filename="${packageName}.zip"`);
+        ctx.body = readFileStream;
+      } catch (error) {
+        console.error('Error happen during the creating module', error);
+        ctx.state.error = 'Error happen during the creating module. Please try late.';
+      }
+
+      try {
+        await fsExtra.remove(folderPath);
+      } catch (error) {
+        console.warn('Error happen during removing temp folder', error);
+      }
+
+      next();
+    })
+    .use(function returnStatic(ctx, next) {
+      if (!ctx.body) {
+        ctx.status = ctx.state.error ? 400 : 200;
+        ctx.body = front.toString();
+      }
+      next();
+    })
+    .use(async function logRequest(ctx, next) {
+      console.info([
+        'req',
+        ctx.response.status,
+        ctx.request.method,
+        [ctx.request.path, ctx.request.querystring].filter(Boolean).join('?')
+      ].join('\t'));
+      next();
+    })
+    .listen(port, () => console.log(`listen to ${port}`));
+})();
